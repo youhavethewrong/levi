3511a797383f15729e0e2e03c5dde9c64f62e368
ESC <youhavethewrong@gmail.com>
2015-09-12 20:58:47 -0500

    Explore memoizing slow method while thinking about a better way to do the matrix distance.

--- src/levi/levenshtein.clj
+++ src/levi/levenshtein.clj
@@ -2,8 +2,6 @@
   (:require [clojure.core.matrix :as matrix]
             [clatrix.core :as cl]))
 
-
-;; TODO, make loop/recur so we can memoize maybe
 (defn slow-distance
   "Calculates the Levenshtein Distance between two strings using a naive
   recursive implementation.
@@ -26,31 +24,40 @@
            (inc (slow-distance s len_s t (dec len_t)))
            (+ cost (slow-distance s (dec len_s) t (dec len_t))))))))
 
-(defn matrix-distance
-  [s t]
-  (let [d (matrix/new-matrix :clatrix (inc (count s))  (inc (count t)))
-        ;; first => m/i
-        d (matrix/emap-indexed #(if (= 0 (second %1)) (first %1) %2) d)
-        ;; second => n/j
-        d (matrix/emap-indexed #(if (= 0 (first %1)) (second %1) %2) d)]
-    (matrix/emap-indexed
-     (fn [k v]
-       (let [i (first k)
-             j (second k)]
-         (if (and (> i 0)
-                  (> j 0))
-           (if (= (.charAt s (dec i))
-                  (.charAt t (dec j)))
-             (nth (nth d (dec i)) (dec j))
-             (let [di (dec i)
-                   dj j
-                   ii i
-                   ij (dec j)
-                   si (dec i)
-                   sj (dec j)
-                   deletion (inc (nth (nth d di) dj))
-                   insertion (inc (nth (nth d ii) ij))
-                   substitution (inc (nth (nth d si) sj))]
-               (min deletion insertion substitution)))
-         v)))
-     d)))
+(def cache (atom {}))
+
+(defn memoize
+  [f s len_s t len_t]
+  (when (not (contains? @cache [s len_s t len_t]))
+    (swap! cache assoc
+           [s len_s t len_t]
+           (f s len_s t len_t)))
+  (get @cache [s len_s t len_t]))
+
+;; TODO don't get screwed by immutability
+;; (defn matrix-distance
+;;   [s t]
+;;   (let [d (matrix/new-matrix :clatrix (inc (count s))  (inc (count t)))
+;;         d (matrix/emap-indexed #(if (= 0 (second %1)) (first %1) %2) d)
+;;         d (matrix/emap-indexed #(if (= 0 (first %1)) (second %1) %2) d)]
+;;     (matrix/emap-indexed
+;;      (fn [k v]
+;;        (let [i (first k)
+;;              j (second k)]
+;;          (if (and (> i 0)
+;;                   (> j 0))
+;;            (if (= (.charAt s (dec i))
+;;                   (.charAt t (dec j)))
+;;              (nth (nth d (dec i)) (dec j))
+;;              (let [di (dec i)
+;;                    dj j
+;;                    ii i
+;;                    ij (dec j)
+;;                    si (dec i)
+;;                    sj (dec j)
+;;                    deletion (inc (nth (nth d di) dj))
+;;                    insertion (inc (nth (nth d ii) ij))
+;;                    substitution (inc (nth (nth d si) sj))]
+;;                (min deletion insertion substitution)))
+;;          v)))
+;;      d)))
